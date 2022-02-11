ce5cf2172005c07c26ce1cca20b8581e63438c75
Jamie Smith <thejamiemsmith@gmail.com>
2022-02-11 11:35:02 -0700

    password section complete

---
+++ password.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+console.log("Welcome to the password validator tool!");
+reader.question("Please enter a password to validate ", function(input) {
+    password = input;
+
+
+    if(password.length >= 10) {
+        console.log("success! password will work") 
+    } else if (password.length < 10) {
+        console.log("error! password is not valid")
+    }
+    reader.close()
+})
