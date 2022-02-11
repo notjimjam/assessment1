ccbe9f618aff757e1fa87296911ff9e128946732
Jamie Smith <thejamiemsmith@gmail.com>
2022-02-11 10:52:16 -0700

    arrays section complete

--- arrays.js
+++ arrays.js
@@ -44,6 +44,11 @@ const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 // CODE HERE
 let answer = []
 
-for(let i = bigOrSmallArray.length;, i > 100; i++){
-  if ()
+for(let i = 0; i < bigOrSmallArray.length; i++) {
+  if (bigOrSmallArray[i] > 100) {
+    answer.push('big')
+  } else if (bigOrSmallArray[i] <= 100) {
+    answer.push('small')
+  }
 }
+console.log(answer)
